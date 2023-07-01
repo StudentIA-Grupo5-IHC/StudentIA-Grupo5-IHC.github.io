20405101899021aaf54a641c99177c341a0d675c
KevinArnoldMuchaHuanca <u201616123@upc.edu.pe>
2023-06-30 19:25:20 -0500

    Update v0.4

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,12 +1,12 @@
 
 AOS.init({
-  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-  offset: 120, // offset (in px) from the original trigger point
-  delay: 0, // values from 0 to 3000, with step 50ms
-  duration: 900, // values from 0 to 3000, with step 50ms
-  easing: 'ease', // default easing for AOS animations
-  once: false, // whether animation should happen only once - while scrolling down
-  mirror: false, // whether elements should animate out while scrolling past them
-  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+  // Configuraciones que se pueden anular por elemento, mediante los atributos `data-aos-*`:
+  offset: 120, // desplazamiento (en px) desde el punto de activación original
+  delay: 0, // valores de 0 a 3000, con paso 50ms
+  duration: 900, // valores de 0 a 3000, con paso 50ms
+  easing: 'ease', // aceleración predeterminada para animaciones AOS
+  once: false, // si la animación debe ocurrir solo una vez, mientras se desplaza hacia abajo
+  mirror: false, // si los elementos deben animarse mientras se desplaza más allá de ellos
+  anchorPlacement: 'top-bottom', // define qué posición del elemento con respecto a la ventana debe activar la animación
 
 });
